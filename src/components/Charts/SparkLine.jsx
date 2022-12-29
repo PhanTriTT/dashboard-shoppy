9a2803d7085dc09f682289448683cc011523995d
Phan Tri <minhtritt01@gmail.com>
2022-12-29 20:24:37 +0700

    fix something

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -19,6 +19,7 @@ const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
       type={type}
       tooltipSettings={{
         visible: true,
+        // eslint-disable-next-line no-template-curly-in-string
         format: '${x} : data ${yval}',
         trackLineSettings: {
           visible: true,
